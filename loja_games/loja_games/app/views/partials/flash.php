68de643f3e49df979846138a1872ac1430f96ba7
narcelo90ff <marcelonassar700@gmail.com>
2026-06-09 08:30:47 -0300

    Delete loja_games/loja_games directory

--- loja_games/loja_games/app/views/partials/flash.php
+++
@@ -1,6 +0,0 @@
-<?php if(!empty($flash)): ?>
-<div class="alert alert-<?= $flash['t'] ?>" id="flash-msg">
-  <span><?= $flash['m'] ?></span>
-  <button onclick="this.parentElement.remove()">✕</button>
-</div>
-<?php endif; ?>
